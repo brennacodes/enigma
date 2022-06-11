f3cdd50f938554598e90a5723ff3724be97680e7
Brenna Stuart <brennacodes@gmail.com>
2022-06-11 14:10:52 -0600

    Feat: getting output to terminal; not encrypting

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,16 +1,19 @@
-require 'helper'
+require_relative 'helper'
 
+enigma = Enigma.new
 
 input_from_file = ARGV[0]
-@message = ''
-ouput_to_file = ARGV[1]
-return key = ARGV[2] if !ARGV[2].nil?
-return date = ARGV[3] if !ARGV[3].nil?
+enigma.output_to_file = ARGV[1]
+return @key = ARGV[2] if !ARGV[2].nil?
+return @date = ARGV[3] if !ARGV[3].nil?
+
 
 File.open(input_from_file, 'r') do |file|
-    @message = input_from_file.read
+    enigma.message = file.read
 end
 
-enigma = Enigma.new(message, output_to_file, key = :itself, date = :itself)
-enigma.encrypt
+encrypt_hash = {encryption: enigma.message, key: enigma.key, date: enigma.date}
+# puts encrypt_hash
+# puts "#{encrypt_hash[:encryption]}"
+enigma.encrypt(encrypt_hash[:encryption])
 enigma.encrypt_output
