aa28ee8ca1951555c79197506feca147b6fe5a96
Brenna Stuart <brennacodes@gmail.com>
2022-06-10 20:32:26 -0600

    Test: Returns todays date pass

--- lib/cryptable.rb
+++ lib/cryptable.rb
@@ -1,3 +1,4 @@
 module Cryptable
+    DATE = Date.today.strftime('%m%d%y')
     
 end
--- spec/cryptable_spec.rb
+++ spec/cryptable_spec.rb
@@ -1,16 +1,16 @@
 require_relative 'spec_helper'
 
 RSpec.describe 'Cryptable' do
-    let!(:object) {Enigma.new}
+    let!(:enigma) {Enigma.new}
 
     it 'exists' do
-        expect(object).to be_instance_of Enigma
+        expect(enigma).to be_instance_of Enigma
     end
 
     it 'can return todays date' do
-        expect(object.date).to eq Date.today.strftime("%d%m%y")
+        expect(Cryptable::DATE).to eq Date.today.strftime("%m%d%y")
     end
-    
 
+ 
   
 end
