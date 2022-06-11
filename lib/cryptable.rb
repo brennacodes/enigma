1ea3d36ae992279e3b35c7b035bb104745de3df4
Brenna Stuart <brennacodes@gmail.com>
2022-06-10 20:40:55 -0600

    Test: return offsets from date; pass

--- lib/cryptable.rb
+++ lib/cryptable.rb
@@ -2,4 +2,8 @@ module Cryptable
     DATE = Date.today.strftime('%m%d%y')
     ALPHABET = ('a'..'z').to_a << " "
     
+
+    def offsets
+        (DATE.to_i ** 2).digits.reverse.last(4)
+    end
 end
